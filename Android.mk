e72a9fa4edbfa255b461a187247de781e3955ce4
Maitreya Patni <56203318+Maitreya29@users.noreply.github.com>
2021-06-24 11:27:57 +0530

    make performance better
    amongus tested

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+# Copyright AshutoshOS 2022
+
 ifeq ($(call my-dir),$(call project-path-for,qcom-wlan))
 
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
@@ -5,4 +7,8 @@ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 include $(call all-subdir-makefiles)
 endif
 
+endif
+
+ifneq ($(PUBG_WIFI),true)
+include $(call all-LAG-FIX)
 endif
